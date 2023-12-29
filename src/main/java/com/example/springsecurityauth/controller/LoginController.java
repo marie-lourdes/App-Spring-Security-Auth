b86150babe16afc502d8ee1f391d43707ece3a63
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-12-29 13:01:34 +0100

    add LoginController

---
+++ src/main/java/com/example/springsecurityauth/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.example.springsecurityauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class LoginController {
+	@GetMapping("/user")
+	public String getUser() {
+		return "Welcome, User";
+	}
+	
+	@GetMapping("/admin")
+	public String getAdmin() {
+		return "Welcome, Admin";
+	}
+}
