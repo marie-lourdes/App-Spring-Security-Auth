404197893c2e606ee62251da148a176dc7e86680
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-12-30 16:56:10 +0100

    add comments

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,5 +6,6 @@ CREATE TABLE dbuser (
   password VARCHAR(250) NOT NULL,
   role VARCHAR(250) NOT NULL
 );
+/*structure et valeur chargé automatiqument dans H2 bdd  par Spring data avec la configuration datasource de application.properties*/ 
 INSERT INTO dbuser (username, password, role) VALUES ('dbuser', '$2a$10$qQxjbJtOlIvn0gV/AUIvQ.g8gKtg7mMY1cxx7ChiKLW7rjSHtJ45m', 'USER'),
 ('dbadmin', '$2a$10$QrYhxG2bksZsWfwJKfSW2eRnvo7ngJXu42kirmYwsu/hVZ8hq79kO', 'ADMIN');
