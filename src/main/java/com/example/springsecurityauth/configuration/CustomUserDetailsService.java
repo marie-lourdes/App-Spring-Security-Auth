ff72bdc60e097658a57ae3fb73f12667d5e40458
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-12-30 16:47:57 +0100

    SpringSecurityConfig.java: add configuration authentication manager with CustomUserDetailsService

--- src/main/java/com/example/springsecurityauth/configuration/CustomUserDetailsService.java
+++ src/main/java/com/example/springsecurityauth/configuration/CustomUserDetailsService.java
@@ -10,11 +10,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.springsecurityauth.model.DBUser;
 import com.example.springsecurityauth.repository.DBUserRepository;
 
-public class CustomDetailsService implements UserDetailsService {
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
 	@Autowired
 	private DBUserRepository dbUserRepository;
 
--- src/main/java/com/example/springsecurityauth/configuration/SpringSecurityConfig.java
+++ src/main/java/com/example/springsecurityauth/configuration/SpringSecurityConfig.java
@@ -1,43 +1,54 @@
 package com.example.springsecurityauth.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
+	@Autowired
+	CustomUserDetailsService customUserDetailsService;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		 http.authorizeHttpRequests(request -> {
+	
+		http.authorizeHttpRequests(request -> {
 			request.requestMatchers("/admin").hasRole("ADMIN");
 			request.requestMatchers("/h2-console/**").permitAll();
 			request.requestMatchers("/user").hasRole("USER");
 			request.anyRequest().authenticated();
-			
-		}).formLogin(Customizer.withDefaults())
-         .csrf(csrf -> csrf
-                 .ignoringRequestMatchers("/h2-console/**"));
-		
-	
+
+		}).formLogin(Customizer.withDefaults()).logout((logout) -> logout.permitAll());
+				
+		http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));
 		return http.build();
 	}
 
+	/*
+	 * @Bean//user en memoire public UserDetailsService users() { UserDetails user =
+	 * User.builder().username("user").password(passwordEncoder().encode("user")).
+	 * roles("USER") .build(); UserDetails admin =
+	 * User.builder().username("admin").password(passwordEncoder().encode("admin"))
+	 * .roles("USER", "ADMIN").build(); return new InMemoryUserDetailsManager(user,
+	 * admin); }
+	 */
+
 	@Bean
-	public UserDetailsService users() {
-		UserDetails user = User.builder().username("user").password(passwordEncoder().encode("user")).roles("USER")
-				.build();
-		UserDetails admin = User.builder().username("admin").password(passwordEncoder().encode("admin"))
-				.roles("USER", "ADMIN").build();
-		return new InMemoryUserDetailsManager(user, admin);
+	public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder)
+			throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder = http
+				.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder.userDetailsService(customUserDetailsService)
+				.passwordEncoder(bCryptPasswordEncoder);
+		return authenticationManagerBuilder.build();
 	}
 
 	@Bean
