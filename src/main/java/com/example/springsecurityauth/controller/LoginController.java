63a6ccfdb21947eb7f26bb5ac9a71686cbb480f5
marie-lourdes <marielourdesleperlier64@gmail.com>
2024-02-17 17:26:47 +0100

    loginController.java: refactor method and route GET getUserInfo(Principal principal, @AuthenticationPrincipal OidcUser oidcUser) with OidcUser and refactor method getOauth2LoginInfo(Principal user,OidcUser oidcUser)  to get OidcIdToken idToken

--- src/main/java/com/example/springsecurityauth/controller/LoginController.java
+++ src/main/java/com/example/springsecurityauth/controller/LoginController.java
@@ -4,10 +4,13 @@ import java.security.Principal;
 import java.util.Map;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,12 +34,12 @@ public class LoginController {
 	}
 
 	@GetMapping("/")
-	public String getUserInfo(Principal principal) {
+	public String getUserInfo(Principal principal, @AuthenticationPrincipal OidcUser oidcUser) {
 		StringBuffer userInfo = new StringBuffer();
 		if (principal instanceof UsernamePasswordAuthenticationToken) {
 			userInfo.append(getUsernamePasswordLoginInfo(principal));
 		} else if (principal instanceof OAuth2AuthenticationToken) {
-			userInfo.append(getOauth2LoginInfo(principal));
+			userInfo.append(getOauth2LoginInfo(principal, oidcUser));
 		}
 		return userInfo.toString();
 
@@ -55,7 +58,7 @@ public class LoginController {
 		return usernameInfo;
 	}
 
-	private StringBuffer getOauth2LoginInfo(Principal user) {
+	private StringBuffer getOauth2LoginInfo(Principal user,OidcUser oidcUser) {
 		   StringBuffer protectedInfo = new StringBuffer();
 		   
 		   OAuth2AuthenticationToken authToken = ((OAuth2AuthenticationToken) user);
@@ -63,12 +66,24 @@ public class LoginController {
 		   if(authToken.isAuthenticated()){
 		   
 		   Map<String,Object> userAttributes = ((DefaultOAuth2User) authToken.getPrincipal()).getAttributes();
-		   
+		   //Token d acces
 		   String userToken = authClient.getAccessToken().getTokenValue();
-		   protectedInfo.append("Welcome, " + userAttributes.get("login")+"<br><br>");
-		 //  protectedInfo.append("e-mail: " + userAttributes.get("email")+"<br><br>");
-		   
-		 //  protectedInfo.append("Access Token: " + userToken+"<br><br>");
+		   protectedInfo.append("Welcome, " + userAttributes.get("login")+"<br><br>"); 
+		   // key login pour authToken du fournisseur d identité  Github et key "name" pour du fournisseur d identité  console google cloud
+		   protectedInfo.append("e-mail: " + userAttributes.get("email")+"<br><br>");
+		   protectedInfo.append("Access Token: " + userToken+"<br><br>");
+			
+		   //Token d identité
+				OidcIdToken idToken = oidcUser.getIdToken();
+				if (idToken != null) {
+					protectedInfo.append("idToken value: " + idToken.getTokenValue() + "<br><br>");
+					protectedInfo.append("Token mapped values <br><br>");
+					Map<String, Object> claims = idToken.getClaims();
+					for (String key : claims.keySet()) {
+						protectedInfo.append("  " + key + ": " + claims.get(key) + "<br>");
+					}
+				}
+			
 		   }
 		   else{
 		   protectedInfo.append("NA");
